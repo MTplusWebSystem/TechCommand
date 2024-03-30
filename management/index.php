c6f9835d16288f4bc7860ec516b5f215477da9c6
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-03-29 21:22:10 -0400

    GER:0.0.3

--- management/index.php
+++ management/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,7 +30,7 @@
             </form>
         </section>
         <section id="logo">
-            <img src="./css/imgs/logo.jpeg" alt="logo">
+            <img src="./css/imgs/logo.png" alt="logo">
         </section>
     </main>
 </body>
