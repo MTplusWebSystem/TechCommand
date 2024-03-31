6509fb8f3eb3a3455e522e91cf6768ce622ce6af
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-03-31 13:47:50 -0400

    PDV:0.0.0

--- sales-page/pages/js/AJAX.js
+++ sales-page/pages/js/AJAX.js
@@ -12,5 +12,15 @@ function Load_Data(){
         }
     });
 }
-
+function Click_Active_Menu() {
+    console.log("click");
+    const bar1 = document.getElementsByClassName("bar1")[0];
+    const bar2 = document.getElementsByClassName("bar2")[0];
+    bar1.style.transform = "rotate(55deg)";
+    bar1.style.position = "absolute";
+    bar2.style.transform = "rotate(-55deg)";
+    bar2.style.position = "absolute";
+    const rm_bar = document.getElementsByClassName("bar3")[0];
+    rm_bar.style.display = "none";
+}
 
