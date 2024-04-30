b6a8931f9730d2dfae4726e797955653ed9f0607
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-04-30 07:53:27 -0400

    v0.0.1

--- controllers/router/endpoints.php
+++ controllers/router/endpoints.php
@@ -2,11 +2,14 @@
 
 $path = [
     "./public/pages/",
+    "./public/styles/",
+    "./public/js/",
+    "./public/assets/"
 ];
 
 $routers = [
-    "/" => "{$path[0]}index",
-    "/login/client" => "{$path[0]}client-login",
+    "/" => "{$path[0]}index.php",
+    "/login/client" => "{$path[0]}client-login.php",
 ];
 
 
--- index.php
+++ index.php
@@ -4,6 +4,85 @@ require "./controllers/router/endpoints.php";
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
+interface routerInterface{
+    public function router($uri);
+}
+
+class AssetsFile
+{
+    private function Error404(){
+        http_response_code(404);
+        require_once("./public/pages/404.php");
+        return;
+    }
+    public function router($uri){
+        global $path;
+        if (substr($uri, 0, 5) == "/css/") {
+            header("Content-Type: text/css");
+            $file = $path[1].substr($uri,5).".css";
+            echo file_get_contents($file);
+
+        }  elseif (substr($uri, 0, 4) == "/js/") {
+            header("Content-Type: application/javascript");
+            $file = $path[2].substr($uri,5).".js";
+            echo file_get_contents($file);
+        } elseif (substr($uri, 0, 8) == "/assets/") {
+            $file = $path[3].substr($uri,8).".svg";
+            if(file_exists($file)){
+                header("Content-Type: image/svg+xml");
+                echo file_get_contents($file);
+            }else{
+                $file = $path[3].substr($uri,8).".jpg";
+                if(file_exists($file)){
+                    header("Content-Type: image/jpg");
+                    echo file_get_contents($file);
+                }else{
+                    $file = $path[3].substr($uri,8).".png";
+                    if(file_exists($file)){
+                        header("Content-Type: image/png");
+                        echo file_get_contents($file);
+                    }else{
+                        $this->Error404();
+                    }
+                }
+            }
+        }else{
+            $this->Error404();
+        }
+    }
+}
+
+class StaticFile implements routerInterface
+{
+    public function router($uri){
+        global $routers;
+        $file = $routers[$uri];
+
+        if(file_exists($file)){
+            require_once($file);
+            return;
+        }else{
+            $assets = new AssetsFile;
+            $assets -> router($uri);
+        }
+    }
+}
+
+class EndPoints{
+    public function router(routerInterface $routerInterface,$uri,$Method){
+        global $requestMethod;
+        if($Method == $requestMethod){
+            $routerInterface -> router($uri);
+        }else{
+            echo "error";
+        }
+    }
+}
+
+$EndPoints = new EndPoints;
+$EndPoints -> router(new StaticFile, $uri , "GET" )
+
+/*
 class Requests{
 
     public function RequestEntry($uri, $path, $page){
@@ -68,6 +147,6 @@ if($requestMethod =="GET"){
         $request->RequestEntry($uri, [$routers[$uri], "php"], true); 
     }
 }
-
+*/
 
 ?>
